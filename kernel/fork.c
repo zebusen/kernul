6f589b1a54e8ef120182fe5574f43a682e031482
zebusen <zebusensei@gmail.com>
2021-08-23 23:51:51 +0800

    kernel: Remove CIB

--- kernel/fork.c
+++ kernel/fork.c
@@ -79,7 +79,6 @@
 #include <linux/sysctl.h>
 #include <linux/kcov.h>
 #include <linux/cpufreq.h>
-#include <linux/cpu_input_boost.h>
 #include <linux/devfreq_boost.h>
 
 #include <asm/pgtable.h>
@@ -1819,7 +1818,6 @@ long _do_fork(unsigned long clone_flags,
 
 	/* Boost CPU to the max for 50 ms when userspace launches an app */
 	if (is_zygote_pid(current->pid)) {
-		cpu_input_boost_kick_max(50);
 		devfreq_boost_kick_max(DEVFREQ_MSM_CPUBW, 50);
 	}
 
